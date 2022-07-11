c52b6ba8a511ef814390b261e6179d679826e8bf
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-07-10 23:16:44 -0400

    [Carlos Retamales] - Eureka

--- backend/Empleados/src/main/java/cl/mingeso/Empleados/EmpleadosApplication.java
+++ backend/Empleados/src/main/java/cl/mingeso/Empleados/EmpleadosApplication.java
@@ -2,8 +2,10 @@ package cl.mingeso.Empleados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmpleadosApplication {
 
 	public static void main(String[] args) {
--- backend/Horarios/src/main/java/cl/mingeso/Horarios/HorariosApplication.java
+++ backend/Horarios/src/main/java/cl/mingeso/Horarios/HorariosApplication.java
@@ -2,8 +2,10 @@ package cl.mingeso.Horarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HorariosApplication {
 
 	public static void main(String[] args) {
